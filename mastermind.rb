f7a3a66dc839ee3e804d79b483bdba86a928ea6d
harkato <92404938+harkato@users.noreply.github.com>
2023-09-14 23:18:07 -0300

    Make changes to start game

--- mastermind.rb
+++ mastermind.rb
@@ -6,20 +6,37 @@ class Game
 
   def initialize
     @attempts = 0
-    generate_code
-    print generate_code
+    code = generate_code
+    print code
   end
 
   def start_game
     puts "Welcome to Mastermind! Your goal is to guess the four colors."
-    puts "Available colors: #{COLORS.join(", ")}"
-    puts "You have a total of #{MAX_ATTEMPTS} attempts."
+    puts "Available colors: #{COLORS.join(", ")} \n"
+
+    until @attempts == MAX_ATTEMPTS
+      execute_game
+      if @correct_guess == @code
+        puts "You Win!"
+        return
+      elsif @attempts == 10
+        puts "You Lose!"
+        return
+      end
+      puts "\nYou have a total of #{MAX_ATTEMPTS - @attempts} attempts. \n" 
+    end
+  end
+
+  def execute_game
+    validate_guess(player_guess)
   end
 
   def player_guess
-    puts "Give your guess:"
-    @guess = []
+    guess = []
     @attempts += 1
+
+    puts "Give your guess\n"
+
     4.times do |i|
       puts "Color #{i+1}:"
       @user_input = gets.chomp.upcase
@@ -27,15 +44,35 @@ class Game
         puts "Invalid input. Please enter a valid color (R, G, B, P, S, or O)."
         @user_input = gets.chomp.upcase
       end
-      @guess << @user_input
+      guess << @user_input
     end
-    print @guess
+    print guess
+    guess
   end
 
+  def validate_guess(guess)
+    @correct_colors = 0
+    @near_colors = 0
+    @correct_guess = []
+    
+    guess.each_with_index do |color, index|
+      if code.include?(color) && color == code[index]
+        @correct_colors += 1
+        @correct_guess[index] = "Check!"
+      elsif code.include?(color)
+        @near_colors += 1
+        @correct_guess[index] = "Near!"
+      else
+        @correct_guess[index] = "Wrong!"
+      end
+    end
 
+    puts "\nHere's your guess: #{@correct_guess.join(", ")}"
+  end
 
   private
 
+  attr_accessor :guess, :code
   def generate_code
     @code = COLORS.sample(4)
   end
@@ -43,4 +80,3 @@ end
 
 game = Game.new
 game.start_game
-game.player_guess
